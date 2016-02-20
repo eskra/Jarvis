724bfae7be2ed03ea7484f144356f87fe556888c
eskra <est.ugolini@gmail.com>
2016-02-20 14:48:49 -0500

    added nlu and nlp files

---
+++ nlu.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+#call Nuance api
+#get sentence from api
+
+#parse sentence
+#look at words in the sentence
+#understand the meaning
+	#based on the meaning, 
+	#call file with input needed
+
+sentence = nuance api
+sentence.split(' ')
+
+if sentence.contains('means')
+	sentence.joins()
+	sentence.split('means')
+	return (addToDictionary(), sentence[0], sentence[2])
