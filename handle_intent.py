e970cec993f0b85ef229d20ae85b8c89c1d29d7c
Tommy Luo <Tommy_Creatine@web084249.wireless.mcgill.ca>
2016-02-21 01:00:39 -0500

    add handle_intent.py

--- handle_intent.py
+++ handle_intent.py
@@ -6,6 +6,13 @@ import get_intent
 import parseOutput
 
 while True:
-	string = parseOutput.getString()
-	# string = raw_input("please enter your input \n")
-	print( get_intent.nlu(string) )
+	#string = parseOutput.getString()
+	string = raw_input("please enter your input \n")
+	result = get_intent.nlu(string)
+	if result[0] == "user.py":
+		# 
+	if result[0] == "addDefinition":
+		definition.addDefinition(result[1], result[2])
+	if result[0] == "getDefinition":
+		definition.getDefinition(result[1])
+	#if result[0] == 
